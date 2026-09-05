441b70ad490adb06ce5d4dd062b48886104d9678
Алескей <manay.ru@mail.ru>
2026-09-05 12:27:50 +1000

    create empty gameObject Game Controller и прикрепили к нему скрипт GameManager

--- MobileDev/Assets/Scripts/GameManager.cs
+++ MobileDev/Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 
@@ -6,5 +7,52 @@ using UnityEngine;
 /// </summary>
 public class GameManager : MonoBehaviour
 {
-    
+    [Tooltip("Ссылка на плитку, которую мы хотим создать")]
+    public Transform tile;
+
+    [Tooltip("Где должна быть размещена первая плитка")]
+    public Vector3 startPoint = new Vector3(0, 0, -5);
+
+    [Tooltip("Сколько плиток мы должны создать в advance")]
+    [Range(1, 15)]
+    public int initSpawnNum = 10;
+
+    /// <summary>
+    /// Где должен быть создан следующий тайл.
+    /// </summary>
+    private Vector3 nextTileLocation;
+
+
+    /// <summary>
+    /// Как должет быть повернут следующий тайл ?
+    /// </summary>
+    private Quaternion nextTileRotation;
+
+    private void Start()
+    {
+        //  Установим нашу начальную точку
+        nextTileLocation = startPoint;
+        nextTileRotation = Quaternion.identity;
+
+        for (int i = 0; i < initSpawnNum; ++i)
+        {
+            SpawnNextTile();
+        }
+    }
+
+
+    /// <summary>
+    /// Создаст тайл в определенном место и
+    /// настроит следующую позицию
+    /// </summary>
+    private void SpawnNextTile()
+    {
+        var newTile = Instantiate(tile, nextTileLocation, nextTileRotation);
+
+        //  Определите, где и под каким углом мы
+        //  должны создать следующий элемент
+        var nextTile = newTile.Find("NextSpawPoint");
+        nextTileLocation = nextTile.position;
+        nextTileRotation = nextTile.rotation;
+    }
 }
