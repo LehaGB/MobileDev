54ba71c4335a206a504073e14fbb102b8386f242
Алескей <manay.ru@mail.ru>
2026-09-06 11:57:58 +1000

    настроил скрипт GameManger, для создания припятствия и добавил префаб в ссылку

--- MobileDev/Assets/Scripts/GameManager.cs
+++ MobileDev/Assets/Scripts/GameManager.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 
 /// <summary>
@@ -10,12 +12,18 @@ public class GameManager : MonoBehaviour
     [Tooltip("Ссылка на плитку, которую мы хотим создать")]
     public Transform tile;
 
+    [Tooltip("Ссылка на препятствие, которую мы хотим создать")]
+    public Transform obstacle;
+
     [Tooltip("Где должна быть размещена первая плитка")]
     public Vector3 startPoint = new Vector3(0, 0, -5);
 
     [Tooltip("Сколько плиток мы должны создать в advance")]
     [Range(1, 15)]
     public int initSpawnNum = 10;
+
+    [Tooltip("Сколько тайлоа создать без препятствий")]
+    public int initNoObstacle = 4;
 
     /// <summary>
     /// Где должен быть создан следующий тайл.
@@ -36,7 +44,7 @@ public class GameManager : MonoBehaviour
 
         for (int i = 0; i < initSpawnNum; ++i)
         {
-            SpawnNextTile();
+            SpawnNextTile(i <= initNoObstacle);
         }
     }
 
@@ -44,8 +52,10 @@ public class GameManager : MonoBehaviour
     /// <summary>
     /// Создаст тайл в определенном место и
     /// настроит следующую позицию
+    ///  <param name="spawnObstacles">Если мы должны создать
+    ///  препятствие</param>
     /// </summary>
-    public void SpawnNextTile()
+    public void SpawnNextTile(bool spawnObstacle = true)
     {
         var newTile = Instantiate(tile, nextTileLocation, nextTileRotation);
 
@@ -54,5 +64,44 @@ public class GameManager : MonoBehaviour
         var nextTile = newTile.Find("NextSpawnPoint");
         nextTileLocation = nextTile.position;
         nextTileRotation = nextTile.rotation;
+
+        if (spawnObstacle)
+        {
+            SpawnObstacle(newTile);
+        }
+    }
+
+
+    private void SpawnObstacle(Transform newTile)
+    {
+        //  Теперь нам нужно получить все возможные места для появления припятстаия.
+        var obstacleSpawnPoints = new List<GameObject>();
+
+        //  Проходим через каждый из дочерних игровых объектов в нашем тайле.
+        foreach (Transform child in newTile)
+        {
+            //  Если у него есть тег ObstacleSpawn.
+            if (child.CompareTag("ObstacleSpawn"))
+            {
+                //  Мы добовляем его как возможность.
+                obstacleSpawnPoints.Add(child.gameObject);
+            }
+            //  Убедись, что есть хотя бы одна.
+            if(obstacleSpawnPoints.Count > 0)
+            {
+                //  Получите случайную точку спауна из тех, что есть.
+                int index = Random.Range(0, obstacleSpawnPoints.Count);
+                var spawnPoint = obstacleSpawnPoints[index];
+
+                //  Сохраните ее позицию для использования.
+                var spawnPos = spawnPoint.transform.position;
+
+                //  Создайте наше припятствие.
+                var newObstacle = Instantiate(obstacle, spawnPos, Quaternion.identity);
+
+                //  Привяжите его к плитке.
+                newObstacle.SetParent(spawnPoint.transform);
+            }
+        }
     }
 }
